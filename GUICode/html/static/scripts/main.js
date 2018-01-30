451956f845e232c0ded44b552eaca06977cfbfd6
Dan DeVere <danieldevere285@gmail.com>
2018-01-30 15:20:33 -0600

    added ability to remove boxes

--- GUICode/html/static/scripts/main.js
+++ GUICode/html/static/scripts/main.js
@@ -1,128 +1,14 @@
-function getCursorPt(x, y) {
-    var svg = document.getElementsByTagName("svg")[0];
-    svg = document.querySelector('svg');
-    var pt = svg.createSVGPoint();
-    pt.x = x;
-    pt.y = y;
-    var cursorpt = pt.matrixTransform(svg.getScreenCTM().inverse());
-    return cursorpt;
-}
-function Box(boxID, pageNum, lineNum, x, y, w, h) {
-    this.boxID = boxID;
-    this.lineNum = lineNum;
-    this.pageNum = pageNum;
-    this.x = new Number(x);
-    this.y = new Number(y);
-    this.w = new Number(w);
-    this.h = new Number(h);
-    this.rect = {};
-    this.dragging = 0;
-    this.handleGroup1;
-    this.handleGroup2;
-    this.handle = new Array();
-    var self = this;
-    this.move1 = function(dx, dy, x, y) {
-        var cursorpt = getCursorPt(x, y);
-        var thisX = new Number(self.rect.attr("x"));
-        var thisY = new Number(self.rect.attr("y"));
-        var height = new Number(self.rect.attr("height"));
-        var width = new Number(self.rect.attr("width"));
-        height = height + thisY - cursorpt.y;
-        width = width + thisX - cursorpt.x;
-        self.rect.attr({
-            x:cursorpt.x,
-            y:cursorpt.y,
-            height: height,
-            width: width
-        });
-        self.handle[0].attr({
-            cx:cursorpt.x,
-            cy:cursorpt.y
-        });
-    }
-    this.move2 = function (dx, dy, x, y) {
-        var cursorpt = getCursorPt(x,y);
-        var thisX = new Number(self.rect.attr("x"));
-        var thisY = new Number(self.rect.attr("y"));
-        var height = new Number(self.rect.attr("height"));
-        var width = new Number(self.rect.attr("width"));
-        height = cursorpt.y - thisY;
-        width = cursorpt.x - thisX;
-        self.rect.attr({
-            height: height,
-            width: width
-        });
-        self.handle[1].attr({
-            cx: cursorpt.x,
-            cy: cursorpt.y
-        });
-    }
-    this.show = function(svg, clickHandle) {
-        this.rect = svg.rect(this.x, this.y, this.w, this.h);
-        this.rect.click(clickHandle);
-        this.rect.data("boxID", self.boxID);
-        var bb = this.rect.getBBox();
-        this.handle[0] = svg.circle(bb.x, bb.y, 20).attr({ class: 'handler' });
-        this.handle[1] = svg.circle(bb.x + bb.width, bb.y + bb.height, 20).attr({ class: 'handler' });
-        this.handle[0].drag(this.move1, null, this.stop);
-        this.handle[1].drag(this.move2, null, this.stop);
-    }
-    this.stop = function() {
-        var x = self.rect.attr("x");
-        var y = self.rect.attr("y");
-        var width = self.rect.attr("width");
-        var height = self.rect.attr("height");
-        self.x = x;
-        self.y = y;
-        self.width = width;
-        self.height = height;
-    }
-}
-function Project(svg, projectName, boxes) {
-    this.svg = svg;
-    this.currentPage = 0;
-    this.boxes = boxes;
-    this.currentBoxes = [];
-    var self = this;
-    this.showBoxes = function() {
-        self.currentBoxes = self.boxes.filter(function (item) { return item.pageNum == self.currentPage; });
-        for (var i in self.currentBoxes) {
-            self.currentBoxes[i].show(self.svg, self.clickBox);
-        }
-    }
-    this.showPage = function(number) {
-        self.currentPage = number;
-        self.svg.clear();
-        self.svg.image(`${projectName}/pages/${number}`);
-        self.showBoxes();
-    }
-    this.clickBox = function(evt) {
-        splitBox(this, evt);
-    }
-    function splitBox(rect, evt) {
-        var boxID = rect.data("boxID");
-        var index = self.boxes.map(function (e) { return e.boxID; }).indexOf(boxID);
-        var box = self.boxes[index];
-        var point = getCursorPt(evt.x, evt.y);
-        var oldWidth = point.x - box.x;
-        var newWidth = box.w - oldWidth;
-        var newBox = new Box('', box.pageNum, box.lineNum, point.x, box.y, newWidth, box.h);
-        box.w = oldWidth;
-        self.boxes.splice(index, 0, newBox);
-        self.showPage(self.currentPage);
-    }
-}
+
 $(document).ready(function() {
     
     $.ajax({
         method: 'GET',
         success: function(data) {
-            var table = '<table><tr><th>Project</th></tr>';
+            var list = '';
             for(var i = 0; i < data.length; i++) {
-                table += `<tr><td class="score">${data[i]}</td></tr>`
+                list += `<a class="dropdown-item project" href = "#" >${data[i]}</a >`;
             }
-            table += '</table>';
-            document.getElementById('table').innerHTML = table;
+            document.getElementById('projects').innerHTML = list;
         },
         error: function(error) {
             console.log(error);
@@ -130,7 +16,7 @@ $(document).ready(function() {
         },
         url: '/projects'
     });
-    $("#table").on('click', '.score', function(event) {
+    $("#projects").on('click', '.project', function(event) {
         var name = $(event.target).text();
         console.log(name);
         var url = `/${name}/boxes`;
@@ -141,7 +27,7 @@ $(document).ready(function() {
                 var boxes = [];
                 console.log(data);
                 for(var i in data) {
-                    var box = new Box(data[i].box_id, data[i].page, data[i].line, data[i].x, data[i].y, data[i].w, data[i].h);
+                    var box = new Box(i, data[i].box_id, data[i].page, data[i].line, data[i].x, data[i].y, data[i].w, data[i].h);
                     boxes.push(box);
                 }
                 var s = Snap("#something");
---
+++ GUICode/html/static/scripts/project.js
@@ -0,0 +1,66 @@
+function Project(svg, projectName, boxes) {
+    this.svg = svg;
+    this.currentPage = 0;
+    this.boxes = boxes;
+    this.currentBoxes = [];
+    this.selectedBox = -1;
+    var self = this;
+    window.onkeyup = function(e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+        if (key == 8 && self.selectedBox != -1) {
+            if(self.boxes[self.selectedBox].selected) {
+                self.boxes.splice(self.selectedBox, 1);
+                for (var i = 0; i < self.boxes.length; i++) {
+                    self.boxes[i].index = i;
+                }
+                self.selectedBox = -1;
+                self.showPage(self.currentPage);
+            }
+        }
+    }
+    this.showBoxes = function () {
+        self.currentBoxes = self.boxes.filter(function (item) { return item.pageNum == self.currentPage; });
+        for (var i in self.currentBoxes) {
+            self.currentBoxes[i].show(self.svg, self.clickBox);
+        }
+    }
+    this.showPage = function (number) {
+        self.currentPage = number;
+        self.svg.clear();
+        self.svg.image(`${projectName}/pages/${number}`);
+        self.showBoxes();
+    }
+    this.clickBox = function (evt) {
+        var index = this.data("index");
+        var box = self.boxes[index];
+        if (self.selectedBox == index && self.boxes[self.selectedBox].selected == true) {
+            splitBox(this, evt);
+        } else {
+            
+            box.selected = true;
+            if (self.selectedBox != -1) {
+                self.boxes[self.selectedBox].selected = false;
+            }
+            self.selectedBox = index;
+            self.showPage(self.currentPage);
+        }
+        
+        
+    }
+    function splitBox(rect, evt) {
+        var index = rect.data("index");
+      //  var index = self.boxes.map(function (e) { return e.boxID; }).indexOf(boxID);
+        var box = self.boxes[index];
+        console.log(box);
+        var point = getCursorPt(evt.clientX, evt.clientY);
+        var oldWidth = point.x - box.x;
+        var newWidth = box.w - oldWidth;
+        var newBox = new Box(index, '', box.pageNum, box.lineNum, point.x, box.y, newWidth, box.h);
+        box.w = oldWidth;
+        self.boxes.splice(index+1, 0, newBox);
+        for(var i = 0; i < self.boxes.length; i++) {
+            self.boxes[i].index = i;
+        }
+        self.showPage(self.currentPage);
+    }
+}
