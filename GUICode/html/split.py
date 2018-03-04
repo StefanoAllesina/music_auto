e3585554c729bf11df58dfa862d1d5b82d1f30c9
symhom <symhom@gmail.com>
2018-03-03 21:52:58 -0600

    edited to match input/output changes

--- GUICode/html/split.py
+++ GUICode/html/split.py
@@ -12,43 +12,34 @@ def convert_page(ff):
     return 0
 
 # Main function of this file
-def split_and_convert(filename, outputfolder, debug = False):
-    reader = PdfFileReader(stream=os.path.abspath(filename))
-    fileN = outputfolder + '/' + 'order_of_pages.csv'
-    
-    with open(fileN, 'w') as csvfile:
-        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for i in range(0, reader.numPages):
-            
 
-            page = reader.pages[i]
-            if debug:
-                print('Converting page: ' + str(i))
-            writer = PdfFileWriter()
-            writer.addPage(page)
-            outputPath = os.path.abspath(outputfolder)
-            # outputFile = outputPath + '/' + os.path.basename(filename)[:-4] + '_' + str(i) + '.pdf'
-            # wr.writerow([i, os.path.basename(filename)[:-4] + '_' + str(i) + '.jpg'])
-            outputFile = outputPath + '/' + 'page'+ '_' + str(i) + '.pdf'
-            wr.writerow([i, 'page' + '_' + str(i) + '.jpg'])
+#replaced filename with path_to_pdf
 
-            f = open(file=outputFile, mode='wb')
-            writer.write(f)
-            f.close()
-            convert_page(outputFile)
+def split_and_convert(path_to_pdf, output_folder, debug = False):
+    reader = PdfFileReader(stream=os.path.abspath(path_to_pdf))
+    for i in range(0, reader.numPages):
+        page = reader.pages[i]
+        if debug:
+            print('Converting page: ' + str(i))
+        writer = PdfFileWriter()
+        writer.addPage(page)
+        outputPath = os.path.abspath(output_folder)
+        outputFile = outputPath + '/' + str(i) + '.pdf'
+        f = open(file=outputFile, mode='wb')
+        writer.write(f)
+        f.close()
+        convert_page(outputFile)
 
     return reader.numPages
 
 
 #This is  for when the script is imported
-def mainFunction(multiplePagesPDF, folder): 
+def mainFunction(path_to_pdf, output_folder): 
     '''
     arg[0] = pdf that is multiple pages long
     arg[1] = folder that you want to place it in
     '''
-    filename = multiplePagesPDF
-    outputfolder = folder
-    numPages = split_and_convert(filename=filename, outputfolder=outputfolder, debug=True)
+    numPages = split_and_convert(filename=path_to_pdf, outputfolder=output_folder, debug=True)
     print("Finished splitting the file!")
     return numPages    
 
@@ -57,8 +48,8 @@ if __name__ == '__main__':
     if len(cl_arguments) < 3:
         raise ValueError("not enough arguments")
     else:
-        filename = cl_arguments[1]
-        outputfolder = cl_arguments[2]
-        split_and_convert(filename=filename, outputfolder=outputfolder, debug=True)
+        path_to_pdf = cl_arguments[1]
+        output_folder = cl_arguments[2]
+        split_and_convert(path_to_pdf, output_folder, debug=True)
         print("Finished splitting the file!")
 
