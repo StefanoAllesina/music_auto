6b1493a5b9f041774d28da4374be1eaf6da25a23
Dan DeVere <danieldevere285@gmail.com>
2018-03-07 00:46:06 -0600

    fixed setup

--- GUICode/html/setup.sh
+++ GUICode/html/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo yum groupinstall "Development Tools"
-sudo yum install ImageMagick python36 python36-devel
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y ImageMagick python36 python36-devel
 sudo pip-3.6 install pipenv
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
 git clone https://github.com/StefanoAllesina/music_auto.git -b release-1.0.0
 cd $PWD/GUICode/html
-pipenv install
+sudo pipenv install
 npm install
-sudo nohup npm start
+sudo nohup PORT=80 npm start &
